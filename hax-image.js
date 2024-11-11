7ecb624a1d14ebe062c82c563cacdf8afe792094
eb0t18 <ethanlevin118@gmail.com>
2024-11-10 22:08:12 -0500

    CheckIn2

---
+++ hax-image.js
@@ -0,0 +1,148 @@
+import { LitElement, html, css } from "lit";
+import { DDDSuper } from "@haxtheweb/d-d-d/d-d-d.js";
+
+
+export class HaxImage extends LitElement {
+
+  constructor() {
+    super();
+    this.title = '';
+    this.description = '';
+    this.created = '';
+    this.lastUpdated = '';
+    this.logo = '';
+    this.slug = 'google.com';
+    this.baseURL = 'google.com';
+  }
+
+  static get properties() {
+    return {
+      title: { type: String },
+      description: { type: String },
+      created: { type: String },
+      lastUpdated: { type: String },
+      logo: { type: String },
+      slug: { type: String },
+      baseURL: { type: String }
+    };
+  }
+
+  static get styles() {
+    return css`
+      .card {
+        display: inline-flex;
+        flex-direction: column;
+        align-items: center;
+        width: 100%;
+        max-width: 320px;
+        border-radius: var(--ddd-radius-md);
+        padding: 16px;
+        background-color: var(--ddd-theme-default-potential0);
+        border: 2px solid var(--ddd-theme-default-potentialMidnight);
+        height: 512px;
+      }
+  
+      .card:hover {
+        box-shadow: 0 6px 16px rgba(0, 0, 0, 0.15);
+        background-color: var(--ddd-theme-default-skyMaxLight);
+      }
+
+      .card:focus {
+        box-shadow: 0 6px 16px rgba(0, 0, 0, 0.15);
+        background-color: var(--ddd-theme-default-navy40        );
+      }
+  
+      .image-container {
+        width: 100%;
+        aspect-ratio: 1.618; 
+        overflow: hidden;
+        border-radius: var(--ddd-radius-rounded);
+        background-color: var(--ddd-theme-default-potential0);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        margin-bottom: 16px;
+      }
+  
+      img {
+        width: 100%;
+        height: auto;
+        object-fit: cover;
+        border-radius: 8px;
+      }
+  
+      .info {
+        margin-top: var(--ddd-spacing-3);
+        font-size: var(--ddd-font-size-xl);
+        font: var(--ddd-font-primary);
+        font-weight: var(--ddd-font-weight-regular);
+        color: var(--ddd-theme-default-navy80);
+        text-align: center;
+        line-height: var(--ddd-lh-auto);
+      }
+  
+      .secondary {
+        font-size: var(--ddd-font-size-m);
+        font: var(--ddd-font-primary);
+        font-weight: var(--ddd-font-weight-regular);
+        color: var(--ddd-theme-default-potential-50);
+        margin-top: 8px;
+        line-height: var(--ddd-lh-auto);
+        text-align: center;
+      }
+  
+      /*.metadata {
+        font-size: var(--ddd-font-size-xxs);
+        color: var(--ddd-theme-default-navy-40);
+        margin-top: 8px;
+        font-style: italic;
+        line-height: var(--ddd-lh-auto);
+        text-align: center;
+      }
+  
+      @media (max-width: 600px) {
+        .card {
+          width: 100%;
+          max-width: 100%;
+          padding: 12px;
+          height: auto; 
+        }
+      }*/
+    `;
+  }
+  
+
+  render() {
+    return html`
+      <div
+        class="card"
+        tabindex="0"
+        @click="${this.openWindow}"
+        @keyup="${this.enter}"
+      >
+        <div class="image-container">
+          <img src="${this.baseURL}/${this.logo}" alt="${this.title}" />
+        </div>
+        <div class="info">${this.title}</div>
+        <div class="secondary">${this.description}</div>
+      </div>
+    `;
+  }
+
+  openWindow() {
+
+      window.open((this.baseURL+'/'+this.slug), '_blank');
+  }  
+
+  enter(e) {
+    if (e.key === 'Enter') {
+      this.openSlug();
+    }
+  }
+
+  static get tag() {
+    return "hax-card";
+  }
+}
+
+customElements.define(HaxImage.tag, HaxImage);
---
+++ hax-search.js
@@ -0,0 +1,136 @@
+import { LitElement, html, css } from 'lit';
+import { DDDSuper } from "@haxtheweb/d-d-d/d-d-d.js";
+import "./hax-image.js";
+
+export class HaxSearch extends LitElement {
+  
+  constructor() {
+    super();
+    this.value = '';
+    this.title = '';
+    this.loading = false;
+    this.items = [];
+    this.jsonUrl = 'https://haxtheweb.org/site.json';
+    this.baseUrl = this.noJsonEnding(this.jsonUrl);
+  }
+  
+  noJsonEnding(url) {
+    return url.replace(/\/?[^\/]*\.json$/, '');
+  }
+
+  static get properties() {
+    return {
+      title: { type: String },
+      loading: { type: Boolean, reflect: true },
+      items: { type: Array },
+      value: { type: String },
+      jsonUrl: { type: String },
+      baseUrl: { type: String },
+    };
+  }
+
+  static get styles() {
+    return css`
+      :host {
+        display: block;
+      }
+
+      .search-wrapper {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: var(--ddd-theme-default-potential50);
+        border-radius: var(--ddd-radius-rounded);
+        border: 2px solid var(--ddd-theme-default-potentialMidnight);
+        padding: 5px 10px;
+        width: 100%;
+        max-width: 400px;
+        margin: 20px auto;
+      }
+
+
+      .analyze-input {
+        font-size: 16px;
+        line-height: var(--ddd-lh-auto);
+      }
+
+      .results {
+        height: 100%;
+      }
+
+
+      input {
+        font-size: 24px;
+        line-height: var(--ddd-lh-auto);
+        width: 100%;
+      }
+    `;
+  }
+
+
+  render() {
+   
+    return html`
+      <h2>${this.title}</h2>
+        <div class="search-wrapper">
+          <input id="input" class="analyze-input" placeholder="https://haxtheweb.org/site.json" @input="${this.inputChanged}" />
+          <div class="search-icon"><button @click="${this.analyze}">Analyze</button></div>
+        </div>
+        <div class="results">
+        ${this.items.map((item) => {
+          const img = item.metadata && item.metadata.files && item.metadata.files[0] ? item.metadata.files[0].url : '';
+
+          return html`
+            <hax-card
+              title="${item.title}"
+              description="${item.description}"
+              logo="${img}"
+              slug="${item.slug}"
+              baseURL="${this.baseUrl}"
+            ></hax-card>
+          `;
+        })}
+      </div>
+      
+    `;
+  }
+
+  inputChanged(e) {
+    this.value = this.shadowRoot.querySelector('#input').value;
+  }
+
+  updated(changedProperties) {
+    if (changedProperties.has('value')) {
+      this.updateResults(this.value);
+    } else if (changedProperties.has('value') && !this.value) {
+      this.items = [];
+    }
+
+    if (changedProperties.has('items') && this.items.length > 0) {
+      console.log(this.items);
+    }
+  }
+
+  updateResults(value) {
+    this.loading = true;
+    this.baseUrl = this.noJsonEnding(this.jsonUrl);
+    fetch(this.jsonUrl)
+      .then(response => response.ok ? response.json() : {})
+      .then(data => {
+        if (data && Array.isArray(data.items)) {
+          this.items = data.items.filter(item => 
+            item.title.toLowerCase().includes(value.toLowerCase()) ||
+            item.description.toLowerCase().includes(value.toLowerCase())
+          );
+        }
+        this.loading = false;
+      });
+  }
+
+
+  static get tag() {
+    return 'hax-search';
+  }
+}
+
+customElements.define(HaxSearch.tag, HaxSearch);
